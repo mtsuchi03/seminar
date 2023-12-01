fa71f423217c07ffdc4690ab2db5b196c09333b7
mtsuchi <cgeg0844@mail3.doshisha.ac.jp>
2023-12-01 21:55:45 +0900

    upgrade

--- code/index.js
+++ code/index.js
@@ -7,3 +7,14 @@ btn.addEventListener('click', function () {
     menu.classList.toggle('active');
     body.classList.toggle('fix');
 });
+//menu btn action
+
+h2 = document.getElementsByTagName('h2');
+agenda = document.getElementById('agenda');
+for (i = 0; i < h2.length; i++) {
+    text = h2[i].textContent.replace(/\n\s+/g, '');
+    text = text.replace(/\d\.\s/, '');
+    h2id = h2[i].parentNode.parentNode.getAttribute('id');
+    agenda.innerHTML += '<li><a href="#' + h2id + '">' + text + '</a></li>'
+}
+//set agenda
